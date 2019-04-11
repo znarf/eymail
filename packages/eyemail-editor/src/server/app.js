d52dddf0ce2a4733be3cdde73ae055cc350f26eb
François Hodierne <francois@hodierne.net>
2019-04-11 09:11:50 +0200

    fix: codemirror path

--- packages/eyemail-editor/src/server/app.js
+++ packages/eyemail-editor/src/server/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public/')));
 app.use(
   '/codemirror',
-  express.static(path.join(__dirname, '../../node_modules/codemirror')),
+  express.static(path.dirname(require.resolve('codemirror/package.json'))),
 );
 
 // routing
