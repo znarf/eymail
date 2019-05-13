0b5f14582ec82ee521592e38c6e881a077aa0339
François Hodierne <francois@hodierne.net>
2019-05-13 17:20:47 +0200

    feat: move html skeleton from Layout to Container

--- packages/eymail-builder/src/layout.js
+++ packages/eymail-builder/src/layout.js
@@ -3,16 +3,7 @@ const PropTypes = require('prop-types');
 
 class Layout extends React.Component {
   render() {
-    return (
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <title>{this.props.title}</title>
-          <style type="text/css">{this.props.style}</style>
-        </head>
-        <body>{this.props.children}</body>
-      </html>
-    );
+    return this.props.children;
   }
 }
 
--- packages/eymail-components/src/container.js
+++ packages/eymail-components/src/container.js
@@ -16,15 +16,30 @@ class Container extends React.Component {
 
   render() {
     return (
-      <BaseBlock innerStyle={this.innerStyle()} classname="Container">
-        {this.props.children}
-      </BaseBlock>
+      <html>
+        <head>
+          <meta charSet="utf-8" />
+          <title>{this.props.title}</title>
+          <style type="text/css">{this.props.style || global.style}</style>
+        </head>
+        <body>
+          <BaseBlock innerStyle={this.innerStyle()} classname="Container">
+            {this.props.children}
+          </BaseBlock>
+        </body>
+      </html>
     );
   }
 }
 
 Container.propTypes = {
+  title: PropTypes.string,
+  style: PropTypes.string,
   children: PropTypes.node.isRequired,
+};
+
+Container.defaultProps = {
+  title: 'Eymail Template',
 };
 
 module.exports = Container;
